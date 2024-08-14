759c553aa20467ab79709557d68a2ede2c30b3e1
Harry Son <harry.son@navercorp.com>
2024-08-13 17:54:10 -0700

    Update cluster name and region

--- patterns/istio/main.tf
+++ patterns/istio/main.tf
@@ -37,8 +37,8 @@ data "aws_availability_zones" "available" {
 }
 
 locals {
-  name   = basename(path.cwd)
-  region = "us-west-2"
+  name   = "arte"
+  region = "us-east-2"
 
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
